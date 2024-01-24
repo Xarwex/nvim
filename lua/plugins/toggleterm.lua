8058d475e84b252a7a70f2dd8a588d2a27c35978
Aleksander <aleksander.kleinszmidt@smarkets.com>
2024-01-24 15:27:39 +0000

    toggleterm kill border

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,9 +1,14 @@
 return {
-	{ "akinsho/toggleterm.nvim", version = "*", opts = {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
     -- shell = function()
     --   return '."' .. vim.fn.system('nu -c "(which nu).path.0"', {}, function (obj)
     --     return obj.stdout
     --   end) .. '"'
     -- end
-  } },
+    float_opts = {
+      border = 'shadow'
+    }
+  }
 }
