79030954f063f4225f35310ad878d77fbc22fce0
Aleksander <aleksander.kleinszmidt@smarkets.com>
2024-01-24 17:07:17 +0000

    toggleterm cols

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,14 +1,20 @@
 return {
   "akinsho/toggleterm.nvim",
+  dependencies = "EdenEast/nightfox.nvim",
   version = "*",
-  opts = {
-    -- shell = function()
-    --   return '."' .. vim.fn.system('nu -c "(which nu).path.0"', {}, function (obj)
-    --     return obj.stdout
-    --   end) .. '"'
-    -- end
-    float_opts = {
-      border = 'shadow'
-    }
-  }
+  config = function(_, _)
+    require "toggleterm".setup({
+      -- shell = function()
+      --   return '."' .. vim.fn.system('nu -c "(which nu).path.0"', {}, function (obj)
+      --     return obj.stdout
+      --   end) .. '"'
+      -- end
+      highlights = {
+        FloatBorder = {
+          guifg = require('nightfox.palette').load('terafox').sel0
+          -- guifg = "#293e40"
+        }
+      }
+    })
+  end,
 }
