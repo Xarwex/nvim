5bbd73e98c8d9a8ef75cd6595446a680178a0cee
Aleksander Kleinszmidt <60325364+Xarwex@users.noreply.github.com>
2024-02-07 21:51:20 +0000

    lsp magic

--- lua/plugins/lsp/mason_lspconfig.lua
+++ lua/plugins/lsp/mason_lspconfig.lua
@@ -8,6 +8,12 @@ return {
 	},
 	config = function(_, opts)
 		require("mason-lspconfig").setup(opts)
+		require("mason-lspconfig").setup_handlers({
+			-- default handler for each server
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
+		})
 	end,
 	cond = not vim.g.vscode,
 }
