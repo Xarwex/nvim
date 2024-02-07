5ad40211c336279713a22d9b250ace961439f489
Aleksander Kleinszmidt <60325364+Xarwex@users.noreply.github.com>
2024-02-07 22:03:53 +0000

    disable toggleterm for vsc

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,20 +1,21 @@
 return {
-  "akinsho/toggleterm.nvim",
-  dependencies = "EdenEast/nightfox.nvim",
-  version = "*",
-  config = function(_, _)
-    require "toggleterm".setup({
-      -- shell = function()
-      --   return '."' .. vim.fn.system('nu -c "(which nu).path.0"', {}, function (obj)
-      --     return obj.stdout
-      --   end) .. '"'
-      -- end
-      highlights = {
-        FloatBorder = {
-          guifg = require('nightfox.palette').load('terafox').sel0
-          -- guifg = "#293e40"
-        }
-      }
-    })
-  end,
+	"akinsho/toggleterm.nvim",
+	dependencies = "EdenEast/nightfox.nvim",
+	version = "*",
+	config = function(_, _)
+		require("toggleterm").setup({
+			-- shell = function()
+			--   return '."' .. vim.fn.system('nu -c "(which nu).path.0"', {}, function (obj)
+			--     return obj.stdout
+			--   end) .. '"'
+			-- end
+			highlights = {
+				FloatBorder = {
+					guifg = require("nightfox.palette").load("terafox").sel0,
+					-- guifg = "#293e40"
+				},
+			},
+		})
+	end,
+	cond = not vim.g.vscode,
 }
