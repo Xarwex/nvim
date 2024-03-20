3515ca66513105aa2c08267608fb3f55001143e0
Aleksander Kleinszmidt <AKleinszmidt@veritionfund.com>
2024-03-20 15:01:17 +0000

    lazy load mason lspconfig

--- lua/plugins/lsp/mason_lspconfig.lua
+++ lua/plugins/lsp/mason_lspconfig.lua
@@ -1,19 +1,20 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-	},
-	opts = {
-		ensure_installed = { "lua_ls", "rust_analyzer" },
-	},
-	config = function(_, opts)
-		require("mason-lspconfig").setup(opts)
-		require("mason-lspconfig").setup_handlers({
-			-- default handler for each server
-			function(server_name)
-				require("lspconfig")[server_name].setup({})
-			end,
-		})
-	end,
-	cond = not vim.g.vscode,
+  "williamboman/mason-lspconfig.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "williamboman/mason.nvim",
+  },
+  opts = {
+    ensure_installed = { "lua_ls", "rust_analyzer" },
+  },
+  config = function(_, opts)
+    require("mason-lspconfig").setup(opts)
+    require("mason-lspconfig").setup_handlers({
+      -- default handler for each server
+      function(server_name)
+        require("lspconfig")[server_name].setup({})
+      end,
+    })
+  end,
+  cond = not vim.g.vscode,
 }
