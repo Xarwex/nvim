f9bed2c26671df1313e88931c6e2d106b38cb7bf
Xarwex <60325364+Xarwex@users.noreply.github.com>
2024-05-06 11:49:48 +0100

    transparent background

--- lua/plugins/themes/catppuccin.lua
+++ lua/plugins/themes/catppuccin.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   opts = {
     flavour = "mocha",
-    transparent_background = false
+    transparent_background = true, 
   },
   config = function(_, opts)
     require("catppuccin").setup(opts)
