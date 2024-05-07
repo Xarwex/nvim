894e930e7a11c08744e415b2965d683d3d857b4c
Aleksander Kleinszmidt <AKleinszmidt@veritionfund.com>
2024-05-07 15:27:51 +0000

    blankline

--- lua/plugins/blankline.lua
+++ lua/plugins/blankline.lua
@@ -1,8 +1,6 @@
 return {
-    "lukas-reineke/indent-blankline.nvim",
-    config = function()
-        require("indent_blankline").setup()
-    end,
-    enabled = false,
-    cond = not vim.g.vscode
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  opts = {},
+  enabled = false
 }
