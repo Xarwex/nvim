e3474d312cc72918af09cc472fecf078236dbb16
Aleksander Kleinszmidt <AKleinszmidt@veritionfund.com>
2024-07-04 17:50:51 +0100

    pylsp -> pyright

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -2,21 +2,22 @@ return {
 	"neovim/nvim-lspconfig",
 	dependencies = { "williamboman/mason-lspconfig.nvim" },
 	event = { "BufReadPre", "BufNewFile", "VeryLazy" },
-	config = function(_, opts)
+	config = function(_, _)
 		local lspconfig = require("lspconfig")
 		-- don't set up RA as it is set up in another extension
 
-		lspconfig.pylsp.setup({
-			settings = {
-				pylsp = {
-					plugins = {
-						pycodestyle = {
-							enabled = false, -- using ruff instead
-						},
-					},
-				},
-			},
-		})
+		-- lspconfig.pylsp.setup({
+		-- 	settings = {
+		-- 		pylsp = {
+		-- 			plugins = {
+		-- 				pycodestyle = {
+		-- 					enabled = false, -- using ruff instead
+		-- 				},
+		-- 			},
+		-- 		},
+		-- 	},
+		-- })
+		lspconfig.pyright.setup({})
 	end,
 	cond = not vim.g.vscode,
 }
