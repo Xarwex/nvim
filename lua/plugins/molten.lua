0e67597b7b0217f600640c2647f1f0c40cf69e8b
Aleksander Kleinszmidt <AKleinszmidt@veritionfund.com>
2024-09-16 14:47:23 +0100

    image + molten skeletons

---
+++ lua/plugins/molten.lua
@@ -0,0 +1,9 @@
+return {
+	"benlubas/molten-nvim",
+	build = ":UpdateRemotePlugins",
+	init = function()
+		-- these are examples, not defaults. Please see the readme
+		-- vim.g.molten_image_provider = "image.nvim"
+		-- vim.g.molten_output_win_max_height = 20
+	end,
+}
