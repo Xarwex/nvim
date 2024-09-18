4b9a7e6fd926c1b138556320f70ba7c08fa834f3
Aleksander Kleinszmidt <AKleinszmidt@veritionfund.com>
2024-09-18 09:09:53 +0100

    move things around

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # NVIM
 
 TODO:
- - figure out why the hell do I have library LSP stuff in my diagnostics
- - cmp crashing sometimes
  - make code actions a bit better, maybe some preview?
 
--- lua/plugins/molten.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"benlubas/molten-nvim",
-	build = ":UpdateRemotePlugins",
-	init = function()
-		-- these are examples, not defaults. Please see the readme
-		-- vim.g.molten_image_provider = "image.nvim"
-		-- vim.g.molten_output_win_max_height = 20
-	end,
-}
